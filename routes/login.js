e0b68b74e424603774c3a367ca7188ded4b9f65a
Nikita <jumper211212@gmail.com>
2018-03-27 02:46:57 +0300

    straight to the finish

--- routes/login.js
+++ routes/login.js
@@ -9,72 +9,34 @@ const listUnspent = promisify(client.listUnspent.bind(client));
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index');
+  res.render('login');
 });
 //=============================================================================================================
 router.post('/', function(req, res, next) {
   const privateKeyString = req.body.pk;
   if(isValidPK(privateKeyString)) {
-    console.log("VALID");
-    //console.log('db', db);
     db.set('addrFromPrKey', bitcore.PrivateKey(privateKeyString).toAddress());
-    //console.log('after set key');
     const WIFkey = bitcore.PrivateKey(privateKeyString).toWIF();
-
-    // (new Promise((resolve, reject) => {
-    //   client.importPrivKey(WIFkey, (err, res) => {
-    //     err ? reject(err) : resolve(res);
-    //   })
-    // }))
-
-
-    // importPrivKey(WIFkey)
-    // .then(res => {
-    //   //console.log('before listUnspent');
-    //   return new Promise((resolve, reject) => {
-    //     client.listUnspent(1, 9999999, [db.get('addrFromPrKey').toString()], (err, res) => {
-    //       err ? reject(err) : resolve(res);
-    //     });
-    //   });
-    // })
     importPrivKey(WIFkey)
     .then(res => {
-      //console.log(res);
       return listUnspent(1, 9999999, [db.get('addrFromPrKey').toString()])
     })
     .then(res => {
-      //console.log(res);
       const balance = res.reduce((acc, current) => {
         acc += current.amount;
         return acc;
       }, 0);
-
-      //console.log('calculating balance', balance);
       db.set('balance', balance);
     })
     .then(() => {
-      res.status(200).send('All ok');
+      res.status(200).send();
     })
     .catch(err => {
-      console.error(err)
+      console.error(err);
     });
-    // client.importPrivKey(WIFkey, function(err,res){
-    //   console.log("IMPORT KEY");
-    //   console.log(err);
-    //   console.log(res);
-    //   client.listUnspent(1, 9999999, [addrFromPrKey.toString()], function(err, res){
-    //     console.log(err);
-    //     console.log(res);
-    //     for(var i=0; i<res.length; i++){
-    //       bal+=res[i].amount;
-    //     }
-    //     console.log(bal);
-    //   });
-    // });
-    // res.status(200).send("All Ok");
   }
   else {
-    res.status(400).send("Private Key is not valid");
+    res.status(400).send('Private Key is not valid');
   }
 });
 
--- routes/send.js
+++ routes/send.js
@@ -7,6 +7,7 @@ const isValidAddress = require('../validation').isValidAddress;
 const listUnspent = promisify(client.listUnspent.bind(client));
 const createRawTransaction = promisify(client.createRawTransaction.bind(client));
 const signRawTransaction = promisify(client.signRawTransaction.bind(client));
+const listTransactions = promisify(client.listTransactions.bind(client));
 
 const fromDataToObject = (sendToAddr, amountToSend, fromAddr, tempAmount, fee) => {
   let obj = {};
@@ -16,7 +17,12 @@ const fromDataToObject = (sendToAddr, amountToSend, fromAddr, tempAmount, fee) =
 };
 
 router.get('/send', function(req, res, next) {
-  res.render('send', { address: db.get('addrFromPrKey'), balance: db.get('balance')});
+  if(db.get('addrFromPrKey') === undefined){
+    //window.location.href = "/";
+    res.render('login');
+  } else {
+    res.render('send', { address: db.get('addrFromPrKey'), balance: db.get('balance')});
+  }
 });
 //=================================================================================================================================
 router.post('/send', function(req, res, next) {
@@ -24,58 +30,28 @@ router.post('/send', function(req, res, next) {
   const fee = 0.0001;
   const sendToAddr = req.body.toAddr
   const amountToSend = +req.body.amount;
+  console.log(amountToSend);
 
-  if(amountToSend+fee<db.get('balance')){
+  if((amountToSend+fee < db.get('balance')) && (amountToSend > 0)){
+    console.log(amountToSend+fee);
+    console.log(db.get('balance'));
     if(isValidAddress(sendToAddr)) {
-      // (new Promise((resolve, reject) => {
-      //   client.listUnspent(1, 9999999, [fromAddr], (err, res) => {
-      //     err ? reject(err) : resolve(res);
-      //   })
-      // }))
-      // .then(res => {
-      //   const listUnspent = res;
-      //   var tempAmount = 0;
-      //   for(var i=0; i<listUnsp.length; i++){
-      //     tempAmount+=listUnsp[i].amount;
-      //
-      //     if(tempAmount > amountToSend + fee){
-      //       console.log("U HAVE ENOUGH MONEY");
-      //       //console.log(sendToAddr);
-      //       function fromDataToObject(sendToAddr, amountToSend, fromAddr) {
-      //         var objj = {};
-      //         objj[sendToAddr] = amountToSend;
-      //         objj[fromAddr] = tempAmount-amountToSend-fee; // back
-      //         return objj;
-      //       }
-      //       console.log(fromDataToObject(sendToAddr, amountToSend, fromAddr));
-      //       return new Promise((resolve, reject) => {
-      //         client.createRawTransaction([{"txid":listUnsp[0].txid, "vout":listUnsp[0].vout}], fromDataToObject(sendToAddr, amountToSend, fromAddr), (err, res) => {
-      //           err ? reject(err) : resolve(res);
-      //         });
-      //       });
-      // })
-
-
-      // client.listUnspent(1, 9999999, [fromAddr], function(err, res){
       listUnspent(1, 9999999, [fromAddr])
       .then(listUnsp => {
-        //console.log(result);
-        // const listUnsp = result;
         let tempAmount = 0;
         let txArray = [];
 
-        for(let i = 0; i < listUnsp.length; i++){
+        for(let i = 0; i < listUnsp.length; i++){ // make txArray
           tempAmount += listUnsp[i].amount;
           txArray.push({"txid":listUnsp[i].txid, "vout":listUnsp[i].vout});
           if(tempAmount > amountToSend + fee){
             console.log("U HAVE ENOUGH MONEY");
-            //console.log(sendToAddr);
             break;
           }
         }
+
         return createRawTransaction(txArray, fromDataToObject(sendToAddr, amountToSend, fromAddr, tempAmount, fee))
       })
-        //console.log(fromDataToObject(sendToAddr, amountToSend, fromAddr));
       .then(response => {
         console.log("CREATE_TRANS");
         console.log(response);
@@ -84,45 +60,35 @@ router.post('/send', function(req, res, next) {
       .then(response => {
         console.log(response);
         res.status(200).send('All ok');
-
-          //res.status(200).send("Ok");
-
-        // if
-       // for
-
-        // res.status(400).send("Not Enough Money");
       }).catch(err => {
         console.log('ERROR', err);
-      })
-
+      });
     }
     else {
-      res.status(400).send("bad address!");
+      res.status(400).send("1");
     }
   } else {
-    res.status(400).send("Not ENOUGH Money");
+    res.status(400).send("2");
   }
-  // listUnspent(1, 99990, [fromAddr])
-  //   .then(res => {
-  //
-  //   })
-
-
 }); // router post
 //===================================================================================================
 router.post('/send/:txid', function(req, res, next) {
   console.log(req.body);
-  client.listTransactions(function(err, response) {
-    console.log(err);
-    console.log(response);
-    for(var i =0; i<response.length; i++){
+  listTransactions()
+  .then(response => {
+    //console.log(response);
+    for(var i = 0; i<response.length; i++){
       if(response[i].txid == req.body.txid){
-        console.log(response[i].confirmations);
+        //console.log(response[i].confirmations);
         db.set('confirmations', response[i].confirmations);
         res.status(200).send(response[i].confirmations.toString());
         break;
       }
     }
+  })
+  .catch(err => {
+    console.error(err);
+    res.status(400).send("Can't see confirmations");
   });
 });
 
